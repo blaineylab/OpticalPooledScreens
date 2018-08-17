2ca57076a6c9f53297869bdb1751baf5ec5571c1
feldman <ebotelho@broadinstitute.org>
2018-08-17 14:06:27 -0400

    install

--- install.sh
+++ install.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-# to reinstall, just delete the env directory
+# first, create the virtual environment using this command: python3 -m venv venv
+# (the second venv/ refers to the folder where the virtual environment is made)
+# (you will have to install the Python 3 virtualenv package if you don't have it)
+
+# to reinstall, just delete the venv directory
+
 
 if [ ! -d "venv" ]; then
-	virtualenv -p python3 env
 	source venv/bin/activate
 	pip install -r requirements.txt
+	pip install -r requirements_skfmm.txt
     # link ops package instead of copying
-    # jupyter and snakemake will import files from ops/ directory
-    pip install -e ops
+    # jupyter and snakemake will import code from .py files in the ops/ directory
+    pip install -e .
 else
     echo "already installed, activating virtual environment (venv)"
-    source env/bin/activate
+    source venv/bin/activate
 fi
 
