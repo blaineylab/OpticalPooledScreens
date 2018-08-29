cd962da1c5d43c8f933f7ad8c8925d1ed5fc8668
feldman <ebotelho@broadinstitute.org>
2018-08-29 01:24:30 -0400

    eek

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@
 
 source venv/bin/activate
 pip install -r requirements.txt
-pip install -r requirements_skfmm.txt
 # link ops package instead of copying
 # jupyter and snakemake will import code from .py files in the ops/ directory
 pip install -e .
