2b6c85825db7391c36060dd46cf8fd94ad002aee
feldman <ebotelho@broadinstitute.org>
2018-09-17 12:34:42 -0400

    imports for notebook

---
+++ ops/imports_ipython.py
@@ -0,0 +1,12 @@
+# plotting for notebook and code reloading 
+
+from ops.imports import *
+from ops.firesnake import Snake
+
+import IPython
+IPython.get_ipython().run_line_magic('load_ext', 'autoreload')
+IPython.get_ipython().run_line_magic('autoreload', '2')
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
