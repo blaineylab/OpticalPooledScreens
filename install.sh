9e5ca7f39abdee36ad4888d51b802720503b8d58
feldman <ebotelho@broadinstitute.org>
2018-11-29 14:05:02 -0500

    sometimes wheel is missing

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # Look for installation instructions in README.md
 
 . venv/bin/activate
+pip install wheel
 pip install -r requirements.txt
 # link ops package instead of copying
 # jupyter and snakemake will import code from .py files in the ops/ directory
