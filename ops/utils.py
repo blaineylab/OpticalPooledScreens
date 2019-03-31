66fead91c336c782cfeee5e83c41fee2b35baf35
feldman4 <feldman4@users.noreply.github.com>
2019-03-31 19:12:38 -0400

    apply parallel tqdn

--- ops/utils.py
+++ ops/utils.py
@@ -1,4 +1,5 @@
 import functools
+import multiprocessing
 
 from string import Formatter
 from itertools import product
@@ -245,7 +246,6 @@ def csv_frame(files_or_search, tqdn=False):
     glob wildcard search term.
     """
     from natsort import natsorted
-    import pandas as pd
     
     def read_csv(f):
         try:
@@ -263,6 +263,20 @@ def csv_frame(files_or_search, tqdn=False):
         return pd.concat([read_csv(f) for f in tqdn(files)], sort=True)
     else:
         return pd.concat([read_csv(f) for f in files], sort=True)
+
+
+def apply_parallel(grouped, func, cpu_count=None, tqdn=True):
+    if cpu_count is None:
+        cpu_count = multiprocessing.cpu_count()
+
+    with multiprocessing.Pool(cpu_count) as p:
+        work = [group for _, group in grouped]
+        if tqdn:
+            results = list(tqdn(p.imap_unordered(func, work), total=len(work)))
+        else:
+            results = list(p.imap_unordered(func, work), total=len(work))
+
+    return pd.concat(results)
 
 
 # NUMPY
